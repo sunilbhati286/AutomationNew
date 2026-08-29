673a5eaad7b78dee6102015b07ad9f2c09cfa929
Sunil <Sunilbhati286@gmail.com>
2026-08-29 16:24:14 +0530

    29 August - Apache Poi Programe

---
+++ src/test/java/ApachePoiExcel/Lab001.java
@@ -0,0 +1,27 @@
+package ApachePoiExcel;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
+public class Lab001 {
+    public static void main(String[] args) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook("/Users/ineel07/Desktop/testData.xlsx");
+       XSSFSheet sheet =  workbook.getSheetAt(0);
+       for (int i = 0; i<sheet.getLastRowNum(); i++){
+           XSSFRow row = sheet.getRow(i);
+           for (int j = 0; j <row.getLastCellNum();j++){
+               XSSFCell cell = row.getCell(j);
+               System.out.print(cell + "\t");
+           }
+           System.out.println();
+       }
+
+
+
+
+     }
+}
